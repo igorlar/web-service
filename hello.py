4590fa4755131eb284ae443f157c57a8ea058d8a
Igor <ig-lar@mail.ru>
2022-12-02 18:46:46 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def show_message():
+    name = request.args.get('name', default = 'USER', type = str)
+    message = request.args.get('message', default = 'HELLO', type = str)
+    return f"<h2>{escape(name)}! {escape(message)}</h2>"
+
